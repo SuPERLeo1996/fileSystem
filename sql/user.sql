af06021f52a9a413cfd0c625066e007825f17923
SuPERLeo1996 <674340679@qq.com>
2019-05-04 20:43:48 +0800

    sql

--- sql/user.sql
+++ sql/user.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 虚拟机
-Source Server Version : 50717
-Source Host           : 192.168.1.104:3306
+Source Server         : SpringMybatis
+Source Server Version : 50155
+Source Host           : 127.0.0.1:3306
 Source Database       : file
 
 Target Server Type    : MYSQL
-Target Server Version : 50717
+Target Server Version : 50155
 File Encoding         : 65001
 
-Date: 2019-02-25 21:58:59
+Date: 2019-05-04 20:43:03
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,8 +31,3 @@ CREATE TABLE `user` (
   `last_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('7bf57743069443a09ab3bc513cfacec9', 'test', '123', '123', '123', null, null, null, null);
